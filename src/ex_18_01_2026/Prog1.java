339b7c2d721af4166013e0cdea6d59d6e5c14494
Vaishnavi <andolevaishnavi01@gmail.com>
2026-01-19 01:02:16 +0530

    comments

---
+++ src/ex_18_01_2026/Prog1.java
@@ -0,0 +1,15 @@
+package ex_18_01_2026;
+
+public class Prog1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/* I am 
+		 * practicing
+		 * java
+		 */
+		System.out.println("Hi how are you");
+
+	}
+
+}
