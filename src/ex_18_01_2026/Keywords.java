fc52492a80fa961c89bd969235c6fca55214c082
Vaishnavi <andolevaishnavi01@gmail.com>
2026-01-27 23:42:24 +0530

    keywords

---
+++ src/ex_18_01_2026/Keywords.java
@@ -0,0 +1,15 @@
+package ex_18_01_2026;
+
+public class Keywords {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ // Do a common math
+		System.out.println(2+2);
+		System.out.println(2-2);
+		System.out.println(2*2);
+		System.out.println(2/2);
+		System.out.println(3/2); //println-> Integer part only it returns not decimal part
+	}
+
+}
